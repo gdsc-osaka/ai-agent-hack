7a48d676ac6552a4acce889ddae66f615c90ad58
Harineko0 <harineko0927@gmail.com>
2025-06-07 18:18:57 +0900

    Fix Dockerfile.preview.db

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
     dialect: "postgresql",
     schema: "./src/db/schema/*",
-    out: "./drizzle",
+    out: "./sql",
     dbCredentials: {
         host: "localhost",
         port: 5432,
