4558a8ba5ad0808afcffa319f63ecc619258b17c
Harineko0 <harineko0927@gmail.com>
2025-06-08 20:32:43 +0900

    Update gen_openapi.sh

--- .scripts/gen_openapi.sh
+++ .scripts/gen_openapi.sh
@@ -4,10 +4,13 @@ set -e
 curl -o tmp.openapi.json "http://localhost:8080/api/openapi"
 
 # Install OpenAPI Generator if not already installed
-openapi-generator --version || brew install openapi-generator
+#openapi-generator --version || brew install openapi-generator # GitHub Actions では brew が使えないため、npm を使用
+openapi-generator-cli version || npm install -g @openapitools/openapi-generator-cli
+
+openapi-generator-cli version-manager set 7.13.0
 
 # Generate TypeScript Client
-openapi-generator generate -i tmp.openapi.json -g typescript-axios -o web/src/openapi
+openapi-generator-cli generate -i tmp.openapi.json -g typescript-axios -o web/src/openapi
 
 # Clean up temporary files
 rm tmp.openapi.json
