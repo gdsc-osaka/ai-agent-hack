a9716ec8f6eb49c22f64fd73f4c53172c3b5ef37
Harineko0 <harineko0927@gmail.com>
2025-06-13 11:25:53 +0900

    Update Dockerfile.preview.db

--- backend/sql/00_init_db.sh
+++ backend/sql/00_init_db.sh
@@ -11,14 +11,14 @@ echo "Database 'auth_db' created"
 
 # 'db'データベースに対してSQLファイルを実行する
 echo "Initializing 'db' database..."
-for f in /docker-entrypoint-initdb.d/01_app/*.sql; do
+for f in /init_sql/01_app/*.sql; do
     echo "Processing $f file..."
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f "$f"
 done
 
 # 'auth_db'データベースに対してSQLファイルを実行する
 echo "Initializing 'auth_db' database..."
-for f in /docker-entrypoint-initdb.d/02_auth/*.sql; do
+for f in /init_sql/02_auth/*.sql; do
     echo "Processing $f file..."
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "auth_db" -f "$f"
 done
