e052d2ce95a26fdf8ac55bc35250add2a5b1ad1c
Harineko0 <harineko0927@gmail.com>
2025-06-13 18:50:52 +0900

    fix lint error

--- web/src/session.ts
+++ web/src/session.ts
@@ -8,27 +8,33 @@ export const getSession = async () => {
   return betterFetch<{
     user: User;
     session: Session;
-  }>((process.env.NEXT_PUBLIC_API_URL ?? process.env.API_URL) + "/api/v1/auth/get-session", {
-    method: "GET",
-    headers: {
-      Cookie: headerStore.get("Cookie") || "",
-    },
-    onRequest: async (ctx) => {
-      const cookieStore = await cookies();
-      const sessionToken = cookieStore.get("auth.session_token")?.value;
-
-      if (sessionToken) {
-        ctx.headers.set("Cookie", `auth.session_token=${sessionToken}`);
-      }
-    },
-    // headers
-    onResponse: async (ctx) => {
-      const setCookie = ctx.response.headers.get("Set-Cookie");
-      const sessionToken = setCookie?.match(/auth\.session_token=([^;]+)/)?.[1];
-      if (sessionToken) {
+  }>(
+    (process.env.NEXT_PUBLIC_API_URL ?? process.env.API_URL) +
+      "/api/v1/auth/get-session",
+    {
+      method: "GET",
+      headers: {
+        Cookie: headerStore.get("Cookie") || "",
+      },
+      onRequest: async (ctx) => {
         const cookieStore = await cookies();
-        cookieStore.set("auth.session_token", sessionToken);
-      }
-    },
-  });
+        const sessionToken = cookieStore.get("auth.session_token")?.value;
+
+        if (sessionToken) {
+          ctx.headers.set("Cookie", `auth.session_token=${sessionToken}`);
+        }
+      },
+      // headers
+      onResponse: async (ctx) => {
+        const setCookie = ctx.response.headers.get("Set-Cookie");
+        const sessionToken = setCookie?.match(
+          /auth\.session_token=([^;]+)/
+        )?.[1];
+        if (sessionToken) {
+          const cookieStore = await cookies();
+          cookieStore.set("auth.session_token", sessionToken);
+        }
+      },
+    }
+  );
 };
