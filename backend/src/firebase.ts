34186c37c36fb7703840b8b289f661dce50d507f
ogatakatsuya <katsuya151225@icloud.com>
2025-06-15 19:22:10 +0900

    init firestore instance

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,12 +1,25 @@
 import * as admin from "firebase-admin";
 import { app } from "firebase-admin";
+import env from "./env";
 
 export default function (FIRE_SA: string): app.App {
   if (admin.apps.length > 0 && admin.apps[0]) {
     return admin.apps[0];
   }
 
-  return admin.initializeApp({
-    credential: admin.credential.cert(FIRE_SA),
+  const firebaseApp =  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(FIRE_SA)),
   });
+
+  if (env.NODE_ENV === "development") {
+    const firestore = admin.firestore(firebaseApp);
+    firestore.settings({
+      host: "localhost:8000",
+      ssl: false,
+    });
+  } else {
+    // for production
+  }
+
+  return firebaseApp;
 }
