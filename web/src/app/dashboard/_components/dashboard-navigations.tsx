1eb871825525830383a636ae60ab5ca3904b250c
Harineko0 <harineko0927@gmail.com>
2025-06-16 01:06:20 +0900

    Add some dashboard pages (draft)

---
+++ web/src/app/dashboard/_components/dashboard-navigations.tsx
@@ -0,0 +1,30 @@
+import { User } from 'better-auth';
+import { Separator } from '@/components/ui/separator';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+export default function({ user, pathname }: {user: User, pathname: string}) {
+  return (
+    <div className={'flex flex-col gap-2'}>
+      <div className={'flex flex-col gap-1 items-start'}>
+        <p className={'text-base font-semibold truncate w-full'}>{user.name}</p>
+        <p className={'text-sm text-gray-700 truncate w-full'}>{user.email}</p>
+      </div>
+      {/* Navigation */}
+      <Separator className={'my-2'}/>
+      <div className={'flex flex-col gap-0.5'}>
+        {[{ label: 'Overview', path: '/dashboard' }, { label: 'Settings', path: '/dashboard/settings' }].map(({ label, path }) => (
+          <Button
+            key={path}
+            variant={path === pathname ? 'outline' : 'ghost'}
+            className={'w-full justify-start'}
+            disabled={path === pathname}
+            asChild
+          >
+            <Link href={path}>{label}</Link>
+          </Button>
+        ))}
+      </div>
+    </div>
+  )
+}
--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,37 +1,57 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../../components/ui/card';
-import { Label } from '../../components/ui/label';
-import { getSession } from '../../session';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
 import SignOutForm from './_components/signout-form';
+import api from '../../api';
+import { headers } from 'next/headers';
+import DashboardNavigations from '@/app/dashboard/_components/dashboard-navigations';
+import { getSession } from '@/session';
 
 export default async function Dashboard() {
-  const { data } = await getSession();
+  const { data, error } = await api(headers).GET("/api/v1/staffs/me/stores");
+  const { data: session, error: sessionError } = await getSession();
 
-  if (!data) {
+  if (error) {
     return (
       <main>
-        <h1>ログインが必要なページ</h1>
+        <h1>${error.code}: ${error.message}</h1>
+        <p>${JSON.stringify(error.details)}</p>
       </main>
     );
   }
 
+  if (sessionError) {
+    return (
+      <main>
+        <h1>${sessionError.status}: ${sessionError.message}</h1>
+      </main>
+    );
+  }
+
+  if (data.length === 0) {
+    // redirect('/dashboard/stores/invites');
+  }
+
   return (
-    <div className={'h-full flex'}>
-      <Card className="w-full max-w-sm m-auto">
-        <CardHeader>
-          <CardTitle>Dashboard</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className={'flex flex-col gap-2'}>
-            <Label>セッション情報</Label>
-            <div>
-              {JSON.stringify(data, null, 2)}
+    <div className={'px-8 py-16 grid grid-cols-1 gap-12 md:grid-cols-4 h-full'}>
+      <DashboardNavigations user={session.user} pathname={'/dashboard'}/>
+      <div className={'md:col-span-3'}>
+        <Card className="w-full h-full">
+          <CardHeader>
+            <CardTitle>Dashboard</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className={'flex flex-col gap-2'}>
+              <Label>Stores</Label>
+              <div>
+                {JSON.stringify(data, null, 2)}
+              </div>
             </div>
-          </div>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <SignOutForm/>
-        </CardFooter>
-      </Card>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <SignOutForm/>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
