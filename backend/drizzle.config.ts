5b05cd33aaf3d96494b2674a8f892281bba717bf
Harineko0 <harineko0927@gmail.com>
2025-06-15 17:00:29 +0900

    Update drizzle.config.ts

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dialect: "postgresql",
-    schema: ["./src/db/schema/users.ts"],
+    schema: ["./src/db/schema/stores.ts"],
     out: "./sql/01_app",
     dbCredentials: {
         host: "localhost",
