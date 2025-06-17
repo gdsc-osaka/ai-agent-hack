3534530dfb203802e93d02f923293e16c433071a
ogatakatsuya <katsuya151225@icloud.com>
2025-06-18 08:24:53 +0900

    feat: customer-repo

---
+++ backend/src/infra/customer-repo.error.ts
@@ -0,0 +1,11 @@
+import { errorBuilder, InferError } from "../shared/error";
+
+export const CustomerAlreadyExistsError = errorBuilder(
+  "CustomerAlreadyExistsError"
+);
+export type CustomerAlreadyExistsError = InferError<
+  typeof CustomerAlreadyExistsError
+>;
+
+export const CustomerNotFoundError = errorBuilder("CustomerNotFoundError");
+export type CustomerNotFoundError = InferError<typeof CustomerNotFoundError>;
---
+++ backend/src/infra/customer-repo.ts
@@ -0,0 +1,42 @@
+import { err, ok, ResultAsync } from "neverthrow";
+import { DBCustomer } from "../domain/customer";
+import { eq } from "drizzle-orm";
+import { customers } from "../db/schema/customers";
+import { DBorTx } from "../db/db";
+import { DBInternalError } from "./shared/db-error";
+import {
+  CustomerAlreadyExistsError,
+  CustomerNotFoundError,
+} from "./customer-repo.error";
+
+export type InsertCustomer = (
+  db: DBorTx
+) => (
+  id: string
+) => ResultAsync<DBCustomer, DBInternalError | CustomerAlreadyExistsError>;
+
+export type FindCustomerById = (
+  db: DBorTx
+) => (
+  id: string
+) => ResultAsync<DBCustomer, DBInternalError | CustomerNotFoundError>;
+
+export const findCustomerById: FindCustomerById = (db) => (id) =>
+  ResultAsync.fromPromise(
+    db.select().from(customers).where(eq(customers.id, id)).limit(1),
+    DBInternalError.handle
+  ).andThen((records) =>
+    records.length > 0
+      ? ok(records[0])
+      : err(CustomerNotFoundError("Customer not found"))
+  );
+
+export const createCustomer: InsertCustomer = (db) => (id) =>
+  ResultAsync.fromPromise(
+    db.insert(customers).values({ id }).returning(),
+    DBInternalError.handle
+  ).andThen((records) =>
+    records.length > 0
+      ? ok(records[0])
+      : err(CustomerAlreadyExistsError("Customer already exists"))
+  );
