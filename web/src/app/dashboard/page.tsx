6ebdb7c0a3f84d147c1be32a09170ab7c4311fc0
Harineko0 <harineko0927@gmail.com>
2025-06-17 15:17:37 +0900

    Add store-item.tsx

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
 import SignOutForm from './_components/signout-form';
 import api from '../../api';
 import { headers } from 'next/headers';
-import DashboardNavigations from '@/app/dashboard/_components/dashboard-navigations';
-import { getSession } from '@/session';
+import StoreItem from '@/app/dashboard/_components/store-item';
 
 export default async function Dashboard() {
-  const { data, error } = await api(headers).GET("/api/v1/staffs/me/stores");
-  const { data: session, error: sessionError } = await getSession();
+  const { data, error } = await api(headers).GET('/api/v1/staffs/me/stores');
 
   if (error) {
     return (
@@ -19,39 +15,14 @@ export default async function Dashboard() {
     );
   }
 
-  if (sessionError) {
-    return (
-      <main>
-        <h1>${sessionError.status}: ${sessionError.message}</h1>
-      </main>
-    );
-  }
-
-  if (data.length === 0) {
-    // redirect('/dashboard/stores/invites');
-  }
-
   return (
-    <div className={'px-8 py-16 grid grid-cols-1 gap-12 md:grid-cols-4 h-full'}>
-      <DashboardNavigations user={session.user} pathname={'/dashboard'}/>
-      <div className={'md:col-span-3'}>
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>Dashboard</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className={'flex flex-col gap-2'}>
-              <Label>Stores</Label>
-              <div>
-                {JSON.stringify(data, null, 2)}
-              </div>
-            </div>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <SignOutForm/>
-          </CardFooter>
-        </Card>
+    <div className={'flex flex-col gap-4'}>
+      <h1 className={'text-2xl font-semibold'}>Dashboard</h1>
+      <h2 className={'text-xl text-outline font-semibold'}>Stores</h2>
+      <div className={'flex flex-col gap-3'}>
+        {data.map((store) => <StoreItem store={store} key={store.id} />)}
       </div>
+      <SignOutForm />
     </div>
   );
 }
