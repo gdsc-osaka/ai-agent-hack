ea5e4e71e8448326bf9a02987ed801329b7615a6
ogatakatsuya <katsuya151225@icloud.com>
2025-06-18 08:53:41 +0900

    feat: firestore-repo

---
+++ backend/src/infra/face-auth-repo.ts
@@ -0,0 +1,62 @@
+import { err, ok, ResultAsync } from "neverthrow";
+import { FieldValue } from "firebase-admin/firestore";
+import { createId } from "@paralleldrive/cuid2";
+import { FaceAuthError } from "./face-auth-repo.error";
+import type { FirebaseApp } from "../firebase";
+import { FirestoreInternalError } from "./shared/firestore-error";
+
+export type RegisterEmbedding = (
+  firebase: FirebaseApp
+) => (embedding: number[]) => ResultAsync<string, FirestoreInternalError>;
+
+export type AuthenticateFace = (
+  firebase: FirebaseApp
+) => (
+  embedding: number[]
+) => ResultAsync<string, FaceAuthError | FirestoreInternalError>;
+
+export const registerEmbedding: RegisterEmbedding = (firebase) => (embedding) =>
+  ResultAsync.fromPromise(
+    (async () => {
+      const firestore = firebase.firestore();
+      const customerId = createId();
+
+      await firestore
+        .collection("embeddings")
+        .doc(customerId)
+        .set({
+          value: FieldValue.vector(embedding),
+          created_at: new Date().toISOString(),
+        });
+
+      return customerId;
+    })(),
+    FirestoreInternalError.handle
+  ).andThen((customerId) => ok(customerId));
+
+export const authenticateFace: AuthenticateFace = (firebase) => (embedding) =>
+  ResultAsync.fromPromise(
+    (async () => {
+      const firestore = firebase.firestore();
+
+      const snapshot = await firestore
+        .collection("embeddings")
+        .findNearest({
+          vectorField: "value",
+          queryVector: FieldValue.vector(embedding),
+          limit: 1,
+          distanceMeasure: "COSINE",
+          distanceThreshold: 0.7,
+          distanceResultField: "vectorDistance",
+        })
+        .get();
+
+      return snapshot;
+    })(),
+    FirestoreInternalError.handle
+  ).andThen((snapshot) => {
+    if (snapshot.empty) {
+      return err(FaceAuthError("No match found"));
+    }
+    return ok(snapshot.docs[0].id);
+  });
