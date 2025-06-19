6cf53971025695edfeb4cb4519f843f8a8aef5bc
Harineko0 <harineko0927@gmail.com>
2025-06-19 21:02:09 +0900

    Add MyOpenAPIHono

--- backend/src/openapi-spec.ts
+++ backend/src/openapi-spec.ts
@@ -1,69 +1,34 @@
-import { openAPISpecs } from "hono-openapi";
-import { Hono } from "hono";
-import { resolver } from "hono-openapi/zod";
-import { ApiError } from "./controller/error/api-error";
-import type { BlankEnv, BlankSchema, Env, Schema } from "hono/dist/types/types";
+import { OpenAPIHono } from "@hono/zod-openapi";
 
-export const openApiSpec = <
-  E extends Env = BlankEnv,
-  S extends Schema = BlankSchema,
-  BasePath extends string = "/",
->(
-  app: Hono<E, S, BasePath>
-) =>
-  openAPISpecs(app, {
-    documentation: {
-      info: {
-        title: "Recall you API",
-        version: "1.0.0",
-        description: "API for Recall you",
-      },
-      servers: [
-        {
-          url: "https://recall-you.web.app",
-          description: "Production Server",
-        },
-        { url: "http://localhost:8080", description: "Local Server" },
-      ],
-      security: [
-        {
-          bearerAuth: [],
-        },
-      ],
-      components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: "http",
-            scheme: "bearer",
-            bearerFormat: "JWT",
-          },
-        },
-      },
+export const MyOpenAPIHono = (args: { docPath: string }) => {
+  const app = new OpenAPIHono();
+
+  app.doc(args.docPath, {
+    openapi: "3.0.0",
+    info: {
+      title: "Recall you API",
+      version: "1.0.0",
+      description: "API for Recall you",
     },
-    defaultOptions: {
-      GET: {
-        responses: {
-          400: {
-            description: "Bad Request",
-            content: {
-              "application/json": {
-                schema: resolver(ApiError),
-              },
-            },
-          },
-        },
+    servers: [
+      {
+        url: "https://recall-you.web.app",
+        description: "Production Server",
       },
-      POST: {
-        responses: {
-          400: {
-            description: "Bad Request",
-            content: {
-              "application/json": {
-                schema: resolver(ApiError),
-              },
-            },
-          },
-        },
+      { url: "http://localhost:8080", description: "Local Server" },
+    ],
+    security: [
+      {
+        session: [],
       },
-    },
+    ],
   });
+
+  app.openAPIRegistry.registerComponent("securitySchemes", "session", {
+    type: "apiKey",
+    in: "cookie",
+    name: "__session",
+  });
+
+  return app;
+};
