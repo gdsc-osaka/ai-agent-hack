62547da1e8c8f2f99a7ed6681294aacdb2227078
Harineko0 <harineko0927@gmail.com>
2025-06-27 11:06:40 +0900

    i18n.ts を追加

---
+++ web/src/i18n.ts
@@ -0,0 +1,23 @@
+type Locales = 'ja' | 'en';
+type DefaultLocale = 'ja';
+
+type I18n<T extends Record<string, unknown>> = {
+  [key in Locales]: T
+}
+
+const createI18n = <
+  T extends Record<Locales, Record<string, unknown>>
+>(
+  i18n: T & { [K in Locales]: T[DefaultLocale] }
+): I18n<T[DefaultLocale]> => {
+  return i18n;
+};
+
+export default createI18n({
+  ja: {
+    hello: 'a',
+  },
+  en: {
+    hello: 'a'
+  },
+})
