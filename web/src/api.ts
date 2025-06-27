6614ed7abd6a413f71d35cb0d9e9d07c8e36e6c1
Harineko0 <harineko0927@gmail.com>
2025-06-27 17:12:10 +0900

    Add api-client.ts, which includes swr-openapi

--- web/src/api.ts
+++ web/src/api.ts
@@ -5,15 +5,16 @@ import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapte
 const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? process.env.API_URL;
 
 // サーバーサイドから呼び出す際は headers を渡す
-export default (
+const api = (
   authorization?: Headers | (() => Promise<ReadonlyHeaders>) | string
 ) =>
   createClient<paths>({
     baseUrl,
     fetch: async (req) => {
-      const headerOrApiKey = typeof authorization === "function"
-        ? await authorization()
-        : authorization;
+      const headerOrApiKey =
+        typeof authorization === "function"
+          ? await authorization()
+          : authorization;
       const mergedHeaders = new Headers(req.headers);
       if (typeof headerOrApiKey === "string") {
         mergedHeaders.set("X-Api-Key", headerOrApiKey);
@@ -30,4 +31,7 @@ export default (
     },
   });
 
+export type Api = ReturnType<typeof api>;
 export type Store = components["schemas"]["Store"];
+
+export default api;
