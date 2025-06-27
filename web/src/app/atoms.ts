d9677931b9c68bed1d70f3778bc98a49e397e4b8
Harineko0 <harineko0927@gmail.com>
2025-06-27 22:35:41 +0900

    format code

--- web/src/app/atoms.ts
+++ web/src/app/atoms.ts
@@ -5,6 +5,11 @@ export type FaceRecognitionState = "no-face" | "face-detected";
 
 export const faceRecognitionAtom = atom<FaceRecognitionState>("no-face");
 
-export const apiKeyAtom = atomWithStorage<string | undefined>("apiKey", undefined, undefined, {
-  getOnInit: true,
-});
+export const apiKeyAtom = atomWithStorage<string | undefined>(
+  "apiKey",
+  undefined,
+  undefined,
+  {
+    getOnInit: true,
+  }
+);
