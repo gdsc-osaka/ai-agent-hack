5668a676ba46fa89b0537a5a06a7851c62e6041a
Harineko0 <harineko0927@gmail.com>
2025-06-28 20:30:42 +0900

    migration ファイルを 0000 のみに Update

--- backend/sql/01_app/0006_melted_talos.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "staff_invitations" ALTER COLUMN "status" SET DATA TYPE text;--> statement-breakpoint
-DROP TYPE "public"."staff_invitation_status";--> statement-breakpoint
-CREATE TYPE "public"."staff_invitation_status" AS ENUM('PENDING', 'ACCEPTED', 'DECLINED');--> statement-breakpoint
-ALTER TABLE "staff_invitations" ALTER COLUMN "status" SET DATA TYPE "public"."staff_invitation_status" USING "status"::"public"."staff_invitation_status";--> statement-breakpoint
-CREATE INDEX "staff_invitations_token_idx" ON "staff_invitations" USING btree ("token");
--- backend/sql/01_app/0007_charming_lady_bullseye.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "customers" ADD COLUMN "tos_accepted_at" timestamp;--> statement-breakpoint
-ALTER TABLE "customers" ADD COLUMN "store_id" varchar(25) NOT NULL;--> statement-breakpoint
-ALTER TABLE "customers" ADD CONSTRAINT "customers_store_id_stores_id_fk" FOREIGN KEY ("store_id") REFERENCES "public"."stores"("id") ON DELETE cascade ON UPDATE no action;
