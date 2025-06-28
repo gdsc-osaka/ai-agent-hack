1082943a45643a1efca411b4398c81b4855a9ee0
Harineko0 <harineko0927@gmail.com>
2025-06-28 15:08:48 +0900

    Add comment to iife function

--- backend/src/shared/func.ts
+++ backend/src/shared/func.ts
@@ -15,4 +15,19 @@ export const voidify = (): void => {
   return undefined;
 };
 
+/**
+ * Immediately Invoked Function Expression: 即時実行関数式
+ * if, try, switch 文などを即時実行し, 擬似的に式として扱うことができるようにする.
+ * @param f
+ * @example
+ * ```typescript
+ * const result = iife(() => {
+ *   if (condition) {
+ *     return "foo";
+ *   } else {
+ *     return "bar";
+ *   }
+ * }
+ * ```
+ */
 export const iife = <T>(f: () => T): T => f();
