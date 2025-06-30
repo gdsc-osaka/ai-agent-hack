4aa90c2b68db8d83c820d1d3b82245c64d1ff6d9
Harineko0 <harineko0927@gmail.com>
2025-06-30 22:25:07 +0900

    favicon, metadata の変更

--- web/src/app/(store)/_components/StoreHeader.tsx
+++ web/src/app/(store)/_components/StoreHeader.tsx
@@ -4,6 +4,7 @@ import { Store } from '@/api';
 import StoreCombobox from '@/app/(store)/_components/StoreCombobox';
 import AccountButton from '@/app/(store)/_components/AccountButton';
 import { User } from 'better-auth';
+import { Button } from '@/components/ui/button';
 
 interface StoreHeaderProps {
   stores: Store[];
@@ -21,6 +22,11 @@ export function StoreHeader({ stores, user, storeId }: StoreHeaderProps) {
         <StoreCombobox stores={stores} initialStoreId={storeId} />
       </div>
       <div className="flex items-center gap-2">
+        <Link href={"/"}>
+          <Button variant={"secondary"}>
+            顔認証画面へ移動
+          </Button>
+        </Link>
         <AccountButton user={user}/>
       </div>
     </header>
